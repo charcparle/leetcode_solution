3594116f4184e994feb73d3040b133a9f240528e
Charles Cheng <charles.yhcheng@gmail.com>
2021-07-13 23:42:03 +0800

    Use two-way pointers to solve "Contain most water"

--- problems.js
+++ problems.js
@@ -112,6 +112,7 @@ testCase=[];
     return max;
 };
 */
+/*
 //attempting Greedy
 var maxArea = function(height) {
     let gMax=0;
@@ -138,6 +139,63 @@ var maxArea = function(height) {
         
     }
     
+    return area;
+};
+*/
+
+/*
+//attempting Greedy #2
+var maxArea = function(height) {
+    let gMax=0;
+    let gMin=height.length;
+    let area=0;
+    let collection={};
+    let keys=[];
+    for (let i=0;i<height.length;i++){
+        if (collection[height[i]]===undefined){
+            collection[height[i]]={max:i,min:i};
+            keys.push(height[i]);
+            //keys = keys.sort((a,b)=>(b-a))
+        } else if (i>collection[height[i]].max){
+            collection[height[i]].max=i;
+        } else if (i<collection[height[i]].min){
+            collection[height[i]].min=i;
+        }
+        // for this to work, the range of x coresponding to the range of y (larger than current y) has to be identified
+        // first by filtering the elements of "keys" with values larger than current y
+        // then the corresponing min and max will be compared to find out the applicable gMin and gMax --> no, the big O will become n^2
+        //let fH = keys.filter(x=>x>i)
+
+        // The max area is found by the lowest  and higest x of the heights (>=currentH)
+        // The question is HOW to get the range of x, because the lowest and highest xs may not come from the same height
+        // in this case, how to obtain the relevant list
+        // or just keep the lowest bound whatever its available:
+        // for each H, go through [heights] to see if the height is larger than currentH; 
+        // the highest x is the current i...?
+
+        // or, just do it with 1st principle
+        area = height[i] * (gMax-gMin);
+    }
+    return area;
+};
+*/
+
+// attempting two-way pointers
+var maxArea = function(height) {
+    let left=0;
+    let right=height.length;
+    let area=0;
+    let collection={};
+    let keys=[];
+    while (left<right) {
+        if (area < Math.min(height[left],height[right])*(right-left)) area = Math.min(height[left],height[right])*(right-left)
+        if (height[left]<=height[right]) {
+            left += 1;
+        } else {
+            right -= 1;
+        }
+        console.log(area)
+    }
     return area;
 };
 
